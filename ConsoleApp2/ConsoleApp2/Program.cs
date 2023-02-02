6f83af8e69d7be823a5f48c3655a3cfbb43233b8
Sylwanas <124230538+Sylwanas@users.noreply.github.com>
2023-02-02 20:43:45 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int StartHp = random.Next(1, 101);
+            Console.WriteLine($"Warrior HP: {StartHp}");
+            Console.WriteLine($"The Regenerate spell is cast!");
+            while (StartHp < 50)
+            {
+                StartHp += 10;
+                Console.WriteLine($"Warrior HP: {StartHp}");
+            }
+            Console.WriteLine($"The Regenerate spell is complete.");
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Xml.Schema;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int roll = 0;
+            int total = 0;
+            do
+            {
+                roll = random.Next(1,7);
+                total += roll;
+                Console.WriteLine($"The player rolls: {roll}");
+            } 
+            while (roll < 6);
+            Console.WriteLine($"Total score: {total}");
+        }
+    }
+}
