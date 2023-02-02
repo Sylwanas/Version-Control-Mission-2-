78406c9c6d12968e04a6094af1583324e154d4eb
Sylwanas <124230538+Sylwanas@users.noreply.github.com>
2023-02-02 20:44:44 +0100

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int d6roll = 0;
+            int d6total = 0;
+            int d10roll = 0;
+            int d10total = 0;
+            int d10roll2 = 0;
+            int d10total2 = 0;
+
+            for (int i = 0; i < 3; i++) 
+            {
+                d6roll = random.Next(1, 7);
+                d6total += d6roll;
+            }
+
+                Console.WriteLine($"A character with strength {d6total} was created.");
+
+            for (int i = 0; i < 8; i++)
+            {
+                d10roll= random.Next(1, 11);
+                d10total+= d10roll;
+            }
+
+                Console.WriteLine($"A gelatinous cube with {d10total + 40} HP appears!");
+
+            for (int i = 0; i < 800; i++)
+            {
+                d10roll2 = random.Next(1, 11);
+                d10total2 += d10roll2;
+            }
+
+                Console.WriteLine($"Dear gods, an army of 100 cubes descends upon us with a total of {d10total2 + 4000} HP. We are doomed!");
+
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            var random = new Random();
+            int tankDistance = random.Next(40, 71);
+            int shells = 20;
+            
+            //Flavor text + userName
+            Console.WriteLine("Lieutenant! Lieutenant wake up! There's a tank coming towards HQ! You don't need to worry though lieutenant, we have 83- *KABOOM* ... 5 shells left! Please you have to save us!\n");
+            Console.WriteLine("Enter your name lieutenant.");
+            string userName = Console.ReadLine();
+            Console.Write("Lieutenant " + userName); Console.Write(" is it? Good, strong name.\n");
+            Console.WriteLine("Now, our chief mapper drew this map for us to use, it's a bit primitive but shows the field at least, good luck!\n");
+
+            //Actual Shooting
+            while (shells > 0)
+            {
+
+                //Lose Condition check
+                if (tankDistance < 1)
+                {
+                    Console.WriteLine("Oh no lieutenant! The enemy have broken through, this is all your fault!");
+                    shells = 0;
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                //Drawing and Moving
+                Console.Write("_/");
+                for (int distanceToTank = 0; distanceToTank < tankDistance - 3; distanceToTank++)
+                {
+                    Console.Write("_");
+                }
+                Console.Write("T");
+                for (int distanceToEnd = tankDistance; distanceToEnd < 80; distanceToEnd++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine("\n");
+                Console.Write("On your command lieutenant " + userName); Console.Write("!\n");
+                int tankMove = random.Next(1, 16);
+
+                Console.Write("Enter distance: ");
+                int aim = Convert.ToInt32(Console.ReadLine());
+                aim -= -3;
+                string shot = new string(' ', aim);
+                Console.Write(shot + "  ");
+                Console.WriteLine("*");
+                shells--;
+
+                tankDistance -= tankMove;
+                Console.Clear();
+
+                if (aim < tankDistance)
+                {
+                    Console.WriteLine("Miss! Too short lieutenant.");
+                    Console.WriteLine($"{shells} shells left.");
+                }
+                if (aim > tankDistance)
+                {
+                    Console.WriteLine("Miss! Too far lieutenant.");
+                    Console.WriteLine($"{shells} shells left.");
+                }
+                if (aim == tankDistance)
+                {
+                    Console.WriteLine("Hit! You did it lieutenant!");
+                    Console.WriteLine("Press enter to go home to your family.");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                if (shells == 0 && aim != tankDistance)
+                {
+                    Console.WriteLine("We're out of shells lieutenant! HQ demands we put you in next, sorry!");
+                }
+            }
+        }
+    }
+}
