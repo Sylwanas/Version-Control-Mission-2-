61a56b958ee112e7fed6200dd0b735aa432182c7
Sylwanas <124230538+Sylwanas@users.noreply.github.com>
2023-02-18 13:49:15 +0100

    Add files via upload

---
+++ Files/Files/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerNamePath = "playername.txt";
+            if (File.Exists(playerNamePath))
+            {
+                string playerName = File.ReadAllText(playerNamePath);
+                Console.WriteLine($"Sup {playerName}, didn't think I'd see you again.");
+            }
+            else
+            {
+                Console.WriteLine("Hey nerd, what's your name?");
+                string playerName = Console.ReadLine();
+                Console.WriteLine($"{playerName} huh? Pretty dumb name.");
+                File.WriteAllText(playerNamePath, playerName);
+            }
+        }
+    }
+}
