f732de60a0c76fcd012e0d27676ed6bbaee34d18
Sylwanas <124230538+Sylwanas@users.noreply.github.com>
2023-02-20 16:04:36 +0100

    Add files via upload

---
+++ Switch1/Switch1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Switch1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Set price: ");
+            string playerPrice = Console.ReadLine();
+            string [] splitInput = playerPrice.Split(' ');
+            decimal x;
+            decimal y;
+            decimal price;
+            decimal lastInput = splitInput.Length -1;
+            x = decimal.Parse(splitInput[0]);
+
+            if (splitInput.Length > 1)
+            {
+                y = decimal.Parse(splitInput[(int)lastInput]);
+
+                switch (splitInput[1]) 
+                {
+                    case "+": 
+                    case "plus":
+                        price = x + y; 
+                        break;
+
+                    case "-": 
+                    case "minus":
+                        price = x - y;
+                        break;
+
+                    case "*":
+                    case "times":
+                        price = x * y;
+                        break;
+
+                    case "/":
+                    case "divided":
+                        price = x / y;
+                        break;
+
+                    default: 
+                        price = x; 
+                        break;
+                }
+            }
+            else 
+            { 
+                price = x;
+            }
+            Console.WriteLine($"Price set to {price}");
+        }
+    }
+}
