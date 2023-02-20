bf95e1ecd9580f8196fab6d543a179f83155d67e
Sylwanas <124230538+Sylwanas@users.noreply.github.com>
2023-02-20 11:40:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Files2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shaggy: Hey there friend, what's your name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine($"Shaggy: {playerName} huh? That's pretty rad.");
+            string[] backers = File.ReadAllLines("Backers.txt");
+            foreach ( string backer in backers ) 
+            { 
+                if (backers.Contains(playerName)) 
+                {
+                    Console.WriteLine($"Shaggy: Zoinks {playerName}! I forgot you were part of the mystery gang, get in the mystery van so we can like, solve some mysteries!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Shaggy: Well it was cool meeting you {playerName}, but me and the gang gotta scram, see you later dude!");
+                    break;
+                }
+            }
+        }
+    }
+}
